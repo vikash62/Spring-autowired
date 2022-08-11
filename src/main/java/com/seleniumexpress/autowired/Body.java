e5a5ed2b8228734eb62b6af0429c59b48bdc243c
Vikash <vikashpathak041@gmail.com>
2022-08-11 23:28:42 +0530

    first commit

--- src/main/java/com/seleniumexpress/autowired/Body.java
+++ src/main/java/com/seleniumexpress/autowired/Body.java
@@ -9,6 +9,6 @@ public class Body {
  public static void main(String[] args) {
 	 ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 	 Human human =  context.getBean("human",Human.class);
-	 human.startPumping();
+	 human.startPumping(); 
 }
 }
